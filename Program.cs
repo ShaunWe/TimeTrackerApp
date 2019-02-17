9a3c6feca0463f37956b4938b20738b1254c235d
Shaun Wehe <raven19528@gmail.com>
2019-02-17 13:29:15 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTrackerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputLine;
+            bool programRunnung = true;
+            string menu = "1. Enter Activity\n" +
+                "2. View Tracked Data\n" +
+                "3. Run Calculations\n" +
+                "4. Exit";
+            
+
+            while (programRunnung)
+            {
+                Console.WriteLine($"Hello {programRunnung.ToString()}\n\nWhat would you like to do today?\n\n{menu}");
+                Console.Write("Selection: ");
+                inputLine = Console.ReadLine().ToLower();
+
+                switch (inputLine)
+                {
+                    case "1":
+                    case "enter activity":
+                        EnterActivity();
+                        break;
+                    case "2":
+                    case "view tracked data":
+                        ViewTrackedData();
+                        break;
+                    case "3":
+                    case "run calculations":
+                        RunCalculations();
+                        break;
+                    case "4":
+                    case "exit":
+                        programRunnung = false;
+                        break;
+                    default:
+                        Console.WriteLine("That is not a valid input.");
+                        Utility.KeyToProceed();
+                        break;
+                }
+            }
+        }
+
+        static void EnterActivity()
+        {
+            Console.WriteLine("Enter Activity.");
+            Utility.KeyToProceed();
+        }
+
+        static void ViewTrackedData()
+        {
+            Console.WriteLine("View Tracked Data");
+            Utility.KeyToProceed();
+        }
+
+        static void RunCalculations()
+        {
+            Console.WriteLine("Run calculations.");
+            Utility.KeyToProceed();
+        }
+    }
+}
---
+++ Utility.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TimeTrackerApp
+{
+    class Utility
+    {
+        public static void KeyToProceed()
+        {
+            //This is a standard pause line
+            Console.WriteLine("Press a key to continue.");
+            Console.ReadKey();
+        }
+
+        public static string displayArray(string[] inArray)
+        {
+            //This method builds a formatted string of the array passed to it
+            string returnString = "{ ";
+
+            if (inArray.Length < 1)
+            {
+                return "{No data}";
+            }
+
+            for (int i = 0; i < inArray.Length; i++)
+            {
+                if (i < inArray.Length - 1)
+                {
+                    returnString = returnString + inArray[i] + ", ";
+                }
+                else
+                {
+                    returnString = returnString + inArray[i] + " ";
+                }
+            }
+
+            returnString += "}";
+
+            return returnString;
+        }
+
+        public static string displayList(IList<string> inList)
+        {
+            //This method builds a formatted string of the list<string> passed to it
+            string returnString = "{ ";
+
+            if (inList.Count < 1)
+            {
+                return "{No data}";
+            }
+
+            for (int i = 0; i < inList.Count; i++)
+            {
+                if (i < inList.Count - 1)
+                {
+                    returnString += inList[i] + ", ";
+                }
+                else
+                {
+                    returnString += inList[i] + " ";
+                }
+            }
+
+            returnString += "}";
+
+            return returnString;
+        }
+    }
+}

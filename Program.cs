069f38f8402e5a7b20b0e64c9c8385c6acaf84f5
Shaun Wehe <raven19528@gmail.com>
2019-02-17 14:12:21 -0500

    Added database interaction
    Added MySql connections and read username into program. Tested and verified working.

--- Program.cs
+++ Program.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace TimeTrackerApp
 {
     class Program
     {
+        /*
+         * Shaun Wehe
+         * Time Tracker App
+         * Project and Portfolio 2
+         * February 17, 2019
+         */
+        static string cs = @"server=10.0.0.101;userid=dbremoteuser;password=password;database=ShaunWehe_MDV229_Database_201902;port=8889";
+
         static void Main(string[] args)
         {
             string inputLine;
@@ -17,11 +26,10 @@ namespace TimeTrackerApp
                 "3. Run Calculations\n" +
                 "4. Exit";
             
-
             while (programRunnung)
             {
                 Console.Clear();
-                Console.WriteLine($"Hello {programRunnung.ToString()}\n\nWhat would you like to do today?\n\n{menu}");
+                Console.WriteLine($"Hello {GetUserName()}\n\nWhat would you like to do today?\n\n{menu}");
                 Console.Write("Selection: ");
                 inputLine = Console.ReadLine().ToLower();
 
@@ -51,8 +59,44 @@ namespace TimeTrackerApp
             }
         }
 
+        static string GetUserName()
+        {
+            string userName = "";
+            MySqlConnection conn = null;
+            try
+            {
+                conn = new MySqlConnection(cs);
+                conn.Open();
+                MySqlDataReader rdr = null;
+
+                string stm = "SELECT user_firstname FROM time_tracker_users WHERE user_id = @userid LIMIT 1";
+                MySqlCommand cmd = new MySqlCommand(stm, conn);
+
+                cmd.Parameters.AddWithValue("@userid", "1");
+                rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    userName = rdr["user_firstname"] as string;
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine($"Error: {ex.ToString()}");
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+            return userName;
+        }
+
         static void EnterActivity()
         {
+            MySqlConnection conn = null;
             Console.Clear();
             Console.WriteLine("Enter Activity.");
             Utility.KeyToProceed();

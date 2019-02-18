2f17737fe3ded2612842979f26351c860d2ac208
Shaun Wehe <raven19528@gmail.com>
2019-02-17 19:10:56 -0500

    Add data option added
    Add data option completed without the actual add to the database. Tested and confirmed working.

--- Program.cs
+++ Program.cs
@@ -37,7 +37,10 @@ namespace TimeTrackerApp
                 {
                     case "1":
                     case "enter activity":
-                        EnterActivity();
+                        while (EnterActivity())
+                        {
+                            //This is empty as the EnterActivity() call returns the boolean value for this
+                        }
                         break;
                     case "2":
                     case "view tracked data":
@@ -63,6 +66,7 @@ namespace TimeTrackerApp
         {
             string userName = "";
             MySqlConnection conn = null;
+
             try
             {
                 conn = new MySqlConnection(cs);
@@ -94,12 +98,214 @@ namespace TimeTrackerApp
             return userName;
         }
 
-        static void EnterActivity()
+        static bool EnterActivity()
         {
             MySqlConnection conn = null;
-            Console.Clear();
-            Console.WriteLine("Enter Activity.");
-            Utility.KeyToProceed();
+            MySqlConnection conn2 = null;
+            MySqlConnection conn3 = null;
+            bool boolReturn = false;
+            try
+            {
+                //Dictionary is used to ensure that the name corresponds to the activity
+                Dictionary<int, string> categories = new Dictionary<int, string>();
+                Dictionary<int, string> descriptions = new Dictionary<int, string>();
+                Dictionary<int, string> dates = new Dictionary<int, string>();
+                string inTransfer, inputLine;
+                
+                bool numberValid = false, newActivitySelectionMade = false;
+                int idValue, categoryChoice, descriptionChoice, dayChoice;
+                double timeValue = 0.0;
+                conn = new MySqlConnection(cs);
+                conn.Open();
+                
+                string stm = "SELECT activity_category_id, category_description FROM activity_categories";
+                MySqlCommand cmd = new MySqlCommand(stm, conn);
+
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    var inTrans = rdr["activity_category_id"];
+                    if (inTrans == null)
+                    {
+                        inTransfer = "unable to read data";
+                    }
+                    else
+                    {
+                        inTransfer = inTrans.ToString();
+                    }
+                    do
+                    {
+                        while (!int.TryParse(inTransfer, out idValue))
+                        {
+                            Console.Write($"Value not recognized as an whole number or already used.\nWhat is the closest whole number value of {inTransfer}: ");
+                            inTransfer = Console.ReadLine();
+                        }
+                    } while (categories.ContainsKey(idValue));
+                    categories.Add(idValue, rdr["category_description"] as string);
+                }
+                categoryChoice = MakeChoice(categories, "Pick a category of activity:\n");
+                if (categoryChoice == -1) { return false; }
+
+                //Reset values for new database read
+                conn2 = new MySqlConnection(cs);
+                conn2.Open();
+                stm = "SELECT activity_descriptions_id, activity_description FROM activity_descriptions";
+                MySqlCommand cmd2 = new MySqlCommand(stm, conn2);
+                MySqlDataReader rdr2 = cmd2.ExecuteReader();
+                
+                while (rdr2.Read())
+                {
+                    var inTrans = rdr2["activity_descriptions_id"];
+                    if (inTrans == null)
+                    {
+                        inTransfer = "unable to read data";
+                    }
+                    else
+                    {
+                        inTransfer = inTrans.ToString();
+                    }
+                    do
+                    {
+                        while (!int.TryParse(inTransfer, out idValue))
+                        {
+                            Console.Write($"Value not recognized as an whole number or already used.\nWhat is the closest whole number value of {inTransfer}: ");
+                            inTransfer = Console.ReadLine();
+                        }
+                    } while (descriptions.ContainsKey(idValue));
+                    descriptions.Add(idValue, rdr2["activity_description"] as string);
+                }
+                descriptionChoice = MakeChoice(descriptions, "Pick a description of the activity.\n");
+                if (descriptionChoice == -1) { return false; }
+
+                //Reset values for new database read
+                conn3 = new MySqlConnection(cs);
+                conn3.Open();
+                stm = "SELECT calendar_date_id, calendar_date FROM tracked_calendar_dates";
+                MySqlCommand cmd3 = new MySqlCommand(stm, conn3);
+                MySqlDataReader rdr3 = cmd3.ExecuteReader();
+
+                while (rdr3.Read())
+                {
+                    var inTrans = rdr3["calendar_date_id"];
+                    if (inTrans == null)
+                    {
+                        inTransfer = "unable to read data";
+                    }
+                    else
+                    {
+                        inTransfer = inTrans.ToString();
+                    }
+                    do
+                    {
+                        while (!int.TryParse(inTransfer, out idValue))
+                        {
+                            Console.Write($"Value not recognized as an whole number or already used.\nWhat is the closest whole number value of {inTransfer}: ");
+                            inTransfer = Console.ReadLine();
+                        }
+                    } while (dates.ContainsKey(idValue));
+                    inTrans = rdr3["calendar_date"];
+                    if (inTrans.GetType() == typeof(DateTime))
+                    {
+                        DateTime temp = (DateTime)inTrans;
+
+                        inTransfer = temp.ToString("yyyy-MM-dd");
+                    }
+                    else
+                    {
+                        inTransfer = inTrans.ToString();
+                    }
+                    dates.Add(idValue, inTransfer);
+                }
+                dayChoice = MakeChoice(dates, "What date did you perform the activity: (Format: Year-Month-Day: 0000-00-00 or select number): ");
+                if (dayChoice == -1) { return false; }
+
+                //This section takes in a time value. It verifies that it is time value based on the quarter hour
+                while (!numberValid)
+                {
+                    Console.Clear();
+                    Console.WriteLine("How many hours did you perform that activity?\n" +
+                        "(Keep in mind that every 15 minutes is 0.25. Must be in 15 minute increments. Format: 0.00)\n" +
+                        "1. Back");
+                    Console.Write("Time: ");
+                    inputLine = Console.ReadLine();
+
+                    if (inputLine == "1" || inputLine.ToLower() == "back")
+                    {
+                        return false;
+                    }
+                    while (!double.TryParse(inputLine, out timeValue))
+                    {
+                        Console.Write("That is not a number value. The value entered must be a number value.\nTime: ");
+                        inputLine = Console.ReadLine();
+                    }
+
+                    if (Math.Abs((timeValue*4) - (int)(timeValue*4)) < (double.Epsilon * 4))
+                    {
+                        numberValid = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("The value must be in quarter value increments and entered as a decimal.\n" +
+                            "Keep in mind that 15 minutes = 0.25, 30 minutes = 0.5, and 45 minutes = 0.75");
+                        Utility.KeyToProceed();
+                    }
+                }
+
+                Console.WriteLine($"Activity entered:\n" +
+                    $"Activity Category: {categories[categoryChoice]}\n" +
+                    $"Activity Description: {descriptions[descriptionChoice]}\n" +
+                    $"Date of Activity: {dates[dayChoice]}\n" +
+                    $"Day of Activity: {dayChoice}\n" +
+                    $"Time Spent: {timeValue}");
+                Utility.KeyToProceed();
+
+                while (!newActivitySelectionMade)
+                {
+                    Console.Clear();
+                    Console.WriteLine("1. Enter another activity\n" +
+                        "2. Back to Main Menu\n");
+                    Console.Write("Selection: ");
+                    inputLine = Console.ReadLine().ToLower();
+
+                    switch (inputLine)
+                    {
+                        case "1":
+                        case "enter another activity":
+                            boolReturn = true;
+                            newActivitySelectionMade = true;
+                            break;
+                        case "2":
+                        case "back to main menu":
+                            newActivitySelectionMade = true;
+                            break;
+                        default:
+                            Console.WriteLine("That is not a recognized selection.");
+                            Utility.KeyToProceed();
+                            break;
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine($"Error: {ex.ToString()}");
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+                if (conn2 != null)
+                {
+                    conn2.Close();
+                }
+                if (conn3 != null)
+                {
+                    conn3.Close();
+                }
+            }
+            return boolReturn;
         }
 
         static void ViewTrackedData()
@@ -114,6 +320,48 @@ namespace TimeTrackerApp
             Console.Clear();
             Console.WriteLine("Run calculations.");
             Utility.KeyToProceed();
+        }
+
+        static int MakeChoice(Dictionary<int,string> input, string callLine)
+        {
+            bool choiceMade = false;
+            string inputLine;
+            int intReturn = 0;
+
+            while (!choiceMade)
+            {
+                Console.Clear();
+                Console.WriteLine($"{callLine}\n");
+                foreach (KeyValuePair<int, string> kvp in input)
+                {
+                    Console.WriteLine($"{kvp.Key}. {kvp.Value}");
+                }
+                Console.WriteLine($"{input.Count + 1}. Back\n");
+                Console.Write("Selection: ");
+                inputLine = Console.ReadLine().ToLower();
+
+                if (inputLine == $"{input.Count + 1}" || inputLine == "back")
+                {
+                    return -1;
+                }
+                foreach (KeyValuePair<int, string> kvp in input)
+                {
+                    if (inputLine == $"{kvp.Key}" || inputLine == kvp.Value.ToLower())
+                    {
+                        intReturn = kvp.Key;
+                        Console.WriteLine($"{kvp.Value} is the chosen value");
+                        Utility.KeyToProceed();
+                        choiceMade = true;
+                        break;
+                    }
+                }
+                if (!choiceMade)
+                {
+                    Console.WriteLine("That is not a recognized entry.");
+                    Utility.KeyToProceed();
+                }
+            }
+            return intReturn;
         }
     }
 }
